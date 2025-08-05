9ddee20458e553ddb63c2dc3f7d47e68b865c37d
MrAyham <h.b.k.ayhm@gmail.com>
2025-08-05 16:59:30 -0400

    Add FAQ section component

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ import Hero from '@/components/sections/hero';
 import About from '@/components/sections/About';
 import ServicesSection from '@/components/sections/ServicesSection';
 import TestimonialsSection from '@/components/sections/TestimonialsSection';
-import CallToAction from '@/components/sections/CallToAction';
+import FaqSection from '@/components/sections/FaqSection';
 import Contact from '@/components/sections/contact';
+import CallToAction from '@/components/sections/CallToAction';
 
 function App() {
   return (
@@ -17,8 +18,9 @@ function App() {
       <About />
       <ServicesSection />
       <TestimonialsSection />
-      <CallToAction />
+      <FaqSection />
       <Contact />
+      <CallToAction />
       <div className="mx-auto max-w-7xl px-4">
         <Footer />
       </div>
